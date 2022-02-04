7d14ff03bf970e54a2850683d738021ef082392d
Ren <ren.t21@icloud.com>
2022-02-04 12:18:38 +0900

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by 高橋蓮 on 2022/02/04.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
